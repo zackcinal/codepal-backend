f01280d469f313bd2667d01b161e9f9f58416d1f
Antonio <afelixj89@gmail.com>
2024-04-10 09:10:00 -0400

    fixed password from being read in the console

--- codepal_app/serializers.py
+++ codepal_app/serializers.py
@@ -3,20 +3,26 @@ from .models import Profile, Project, Review, Like, Follow
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    
+    
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'username', 'password')
+        extra_kwargs = {
+            'password': {'write_only': True},  # Specify password field as write-only
+        }
+       
 
     def create(self, validated_data):
-      print(validated_data)
-      user = User.objects.create_user(
-        first_name = validated_data['first_name'],
-        last_name = validated_data['last_name'],
-        username=validated_data['username'],
-        password=validated_data['password']
-      )
-      print(user)
-      return user
+        user = User.objects.create_user(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        print(user)
+        return user
+
       
 
 class ProfileSerializer(serializers.ModelSerializer):
