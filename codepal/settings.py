a5a7bfecccfc6c09a9dcaf688141ebc122df959c
Antonio <afelixj89@gmail.com>
2024-04-19 08:24:04 -0400

    make it better

--- codepal/settings.py
+++ codepal/settings.py
@@ -55,7 +55,7 @@ SECRET_KEY = 'django-insecure-xm%pzs-f=79=sy7=e0dyi4k5qqwgd8_7sv-@)r_xg)vul7!2!m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://codepalga-5698f11ed231.herokuapp.com/',"http://localhost:3000", '127.0.0.1', 'http://127.0.0.1:8000/, ']
+ALLOWED_HOSTS = ['codepalga-5698f11ed231.herokuapp.com/',"http://localhost:3000", '127.0.0.1', 'http://127.0.0.1:8000/']
 
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 # Application definition
