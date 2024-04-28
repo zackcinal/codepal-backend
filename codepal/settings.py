f9babbc1c47fe6869aa8baa722dddaf3f7d082fc
Antonio <afelixj89@gmail.com>
2024-04-28 19:21:45 -0400

    gitignored env file

--- codepal/settings.py
+++ codepal/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 from whitenoise.storage import CompressedManifestStaticFilesStorage
-from codepal.env import SECRET_KEY
+from env import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
